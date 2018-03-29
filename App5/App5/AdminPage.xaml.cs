50fe8cc09563986b956bc249243b6cabdd269f6e
Radchenko Evgeniy <capta@CAPTAIN-PC>
2018-03-29 21:44:02 +1000

    set rights from admin panel

--- App5/App5/AdminPage.xaml.cs
+++ App5/App5/AdminPage.xaml.cs
@@ -37,7 +37,7 @@ namespace App5
             Player selectedPlayer = e.Item as Player;
             if (selectedPlayer != null)
             {
-                string action = await DisplayActionSheet("Действия", "Отмена", null, "Информация", "Редактировать", "Кикнуть", "Забанить");
+                string action = await DisplayActionSheet("Действия", "Отмена", null, "Информация", "Редактировать", "Кикнуть", "Забанить", "Предоставить права админа");
                 PlayerAction(action, selectedPlayer);
             }
         }
@@ -54,6 +54,14 @@ namespace App5
                     break;
                 case "Кикнуть":
                     string rsp = Methods.POST_request(JsonConvert.SerializeObject(new SocketReq { user = player.user, room = room}), "kick-user");
+                    await DisplayAlert("Responce", rsp, "OK");
+                    break;
+                case "Предоставить права админа":
+                    rsp = Methods.POST_request(JsonConvert.SerializeObject(new SocketReq { user = player.user, action = "admin"}), "give-permission");
+                    await DisplayAlert("Responce", rsp, "OK");
+                    break;
+                case "Предоставить права пользователя":
+                    rsp = Methods.POST_request(JsonConvert.SerializeObject(new SocketReq { user = player.user, action = "user" }), "give-permission");
                     await DisplayAlert("Responce", rsp, "OK");
                     break;
 
